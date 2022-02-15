d12b3ad4c973201a976040fde5dd92505c4cc8b7
AngelMathew <AngelMathew@users.noreply.github.com>
2022-02-15 20:31:32 +0000

    deployed via Gatsby Publish Action 🎩 for 04af4637dee5fab37f5988b07b70126221d96444

---
+++ component---src-pages-404-js-0f841d702f003e2fe49a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkangel_anna_mathew=self.webpackChunkangel_anna_mathew||[]).push([[883],{9616:function(e,n,t){"use strict";t.r(n);var a=t(7294),l=t(7198),u=t(3751);n.default=function(){return a.createElement(l.Z,null,a.createElement(u.Z,{title:"404: Not found"}),a.createElement("h1",null,"404: Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0f841d702f003e2fe49a.js.map
---
+++ component---src-pages-page-2-js-a124b4018dc2706fea85.js
@@ -0,0 +1,2 @@
+(self.webpackChunkangel_anna_mathew=self.webpackChunkangel_anna_mathew||[]).push([[617],{9863:function(e,t,n){"use strict";n.r(t);var a=n(7294),l=n(5444),c=n(7198),o=n(3751);t.default=function(){return a.createElement(c.Z,null,a.createElement(o.Z,{title:"Page two"}),a.createElement("h1",null,"Hi from the second page"),a.createElement("p",null,"Welcome to page 2"),a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-a124b4018dc2706fea85.js.map
