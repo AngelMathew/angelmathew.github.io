127b96930b9e867861a16c76fe3a2f08775d4c3c
AngelMathew <AngelMathew@users.noreply.github.com>
2025-03-12 17:14:09 +0000

    deployed via Gatsby Publish Action 🎩 for 4f0e91db021d8af67b18dbc96826da4b06e5b876

---
+++ component---src-pages-404-js-548ad01580d4eb6c6749.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkangel_anna_mathew=self.webpackChunkangel_anna_mathew||[]).push([[125],{3331:function(e,t,n){n.r(t);var a=n(6540),l=n(4794),c=n(3895),r=n(2269);t.default=()=>a.createElement(c.A,null,a.createElement(r.A,{title:"404: Not found"}),a.createElement("div",{style:{textAlign:"center",marginTop:"2rem"}},a.createElement("h1",null,"404: Not Found"),a.createElement(l.Link,{to:"/"},"Go back to home")))}}]);
+//# sourceMappingURL=component---src-pages-404-js-548ad01580d4eb6c6749.js.map
